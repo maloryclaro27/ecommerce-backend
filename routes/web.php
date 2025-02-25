f06e1f411edf86d6b8bd556740eea818f00f99d3
Malory Claro <134218250+maloryclaro27@users.noreply.github.com>
2025-02-25 15:29:05 -0500

    Rutas basicas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    echo "LISTADO DE PRODUCTOS DE ECOMMERCE";
+});
+
+Route::get('/products/{name}', function ($name) {
+    echo "El producto es: $name";
+});
+
+Route::get('/products/create', function ($name) {
+    echo "El producto es: $name";
+});
+
+
+
